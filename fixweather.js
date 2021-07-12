79ccb8c7b57572bff1f609e74337bec747214a0b
Raymond Camden <raymondcamden@gmail.com>
2021-07-12 09:37:50 -0500

    test

--- fixweather.js
+++ fixweather.js
@@ -1,4 +1,4 @@
-import { readJSON, writeJSON, removeFile } from 'https://deno.land/x/flat@0.0.10/mod.ts' 
+import { readJSON, writeJSON, removeFile } from 'https://deno.land/x/flat@0.0.11/mod.ts' 
 
 const filename = Deno.args[0];
 const json = await readJSON(filename);
