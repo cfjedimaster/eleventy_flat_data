1c1f1cf6fb12d39e2ec7cb96127e4c45b831e936
Raymond Camden <raymondcamden@gmail.com>
2021-07-12 10:29:30 -0500

    test

--- fixweather.js
+++ fixweather.js
@@ -2,4 +2,17 @@ import { readJSON, writeJSON, removeFile } from 'https://deno.land/x/flat@0.0.11
 
 const filename = Deno.args[0];
 const json = await readJSON(filename);
+json.temperature = fixC(json.temperature);
+
+json.forecast.forEach(f => {
+	f.temperature = fixC(f.temperature);
+});
+
 console.log(json);
+
+await writeJSON(filename, json);
+
+function fixC(s) {
+	let tempInC = s.split(' ')[0];
+	return (tempInC * 1.8 + 32).toFixed(0);
+}
